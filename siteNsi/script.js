c9997dc7946732310434a6bda482371f8db226b6
GnomeApprenti <42942956+GnomeApprenti@users.noreply.github.com>
2019-12-01 00:05:26 +0100

    Ajouts mineurs
    -opacité logos hover
    -premiers test lancement audio

--- siteNsi/script.js
+++ siteNsi/script.js
@@ -1,5 +1,95 @@
-var element = document.getElementById('clickme');
+var player = document.querySelector('#audioPlayer');
+
+player.addEventListener('click', function(){
+  player.play();
+});
+
+var element = document.getElementById('musique1');
 
 element.addEventListener('click', function(e){
-  e.target.innerHTML = 'bravo chacal';
+  alert("lance la musique 1 chacal")
+});
+
+var element = document.getElementById('musique2');
+
+element.addEventListener('click', function(e){
+  alert("lance la musique 2 chacal")
+});
+
+var element = document.getElementById('musique3');
+
+element.addEventListener('click', function(e){
+  alert("lance la musique 3 chacal")
+});
+
+var element = document.getElementById('musique4');
+
+element.addEventListener('click', function(e){
+  alert("lance la musique 4 chacal")
+});
+
+var element = document.getElementById('musique5');
+
+element.addEventListener('click', function(e){
+  alert("lance la musique 5 chacal")
+});
+
+var element = document.getElementById('musique6');
+
+element.addEventListener('click', function(e){
+  alert("lance la musique 6 chacal")
+});
+
+var element = document.getElementById('musique7');
+
+element.addEventListener('click', function(e){
+  alert("lance la musique 7 chacal")
+});
+
+var element = document.getElementById('musique8');
+
+element.addEventListener('click', function(e){
+  alert("lance la musique 8 chacal")
+});
+
+var element = document.getElementById('musique9');
+
+element.addEventListener('click', function(e){
+  alert("lance la musique 9 chacal")
+});
+
+var element = document.getElementById('musique10');
+
+element.addEventListener('click', function(e){
+  alert("lance la musique 10 chacal")
+});
+
+var element = document.getElementById('musique11');
+
+element.addEventListener('click', function(e){
+  alert("lance la musique 11 chacal")
+});
+
+var element = document.getElementById('musique12');
+
+element.addEventListener('click', function(e){
+  alert("lance la musique 12 chacal")
+});
+
+var element = document.getElementById('musique13');
+
+element.addEventListener('click', function(e){
+  alert("lance la musique 13 chacal")
+});
+
+var element = document.getElementById('musique14');
+
+element.addEventListener('click', function(e){
+  alert("lance la musique 14 chacal")
+});
+
+var element = document.getElementById('musique15');
+
+element.addEventListener('click', function(e){
+  alert("lance la musique 15 chacal")
 });
