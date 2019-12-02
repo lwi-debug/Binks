a16f85f4faab9b22c8cb26cccc6a14d19394e747
GnomeApprenti <42942956+GnomeApprenti@users.noreply.github.com>
2019-12-02 18:13:29 +0100

    Ajout des musiques
    -Ajout des musiques on click du logo

--- siteNsi/script.js
+++ siteNsi/script.js
@@ -1,95 +1,21 @@
-var player = document.querySelector('#audioPlayer');
+function play(idPlayer, control) {
 
-player.addEventListener('click', function(){
-  player.play();
-});
+  var player = document.querySelector('#' + idPlayer);
 
-var element = document.getElementById('musique1');
+  if(player.paused){
+    player.play();
+    control.textContent = 'pause';
+  }
+  else{
+    player.pause();
+    control.textContent = 'play';
+  }
+}
 
-element.addEventListener('click', function(e){
-  alert("lance la musique 1 chacal")
-});
+function resume(idPlayer) {
+  
+    var player = document.querySelector('#' + idPlayer);
 
-var element = document.getElementById('musique2');
-
-element.addEventListener('click', function(e){
-  alert("lance la musique 2 chacal")
-});
-
-var element = document.getElementById('musique3');
-
-element.addEventListener('click', function(e){
-  alert("lance la musique 3 chacal")
-});
-
-var element = document.getElementById('musique4');
-
-element.addEventListener('click', function(e){
-  alert("lance la musique 4 chacal")
-});
-
-var element = document.getElementById('musique5');
-
-element.addEventListener('click', function(e){
-  alert("lance la musique 5 chacal")
-});
-
-var element = document.getElementById('musique6');
-
-element.addEventListener('click', function(e){
-  alert("lance la musique 6 chacal")
-});
-
-var element = document.getElementById('musique7');
-
-element.addEventListener('click', function(e){
-  alert("lance la musique 7 chacal")
-});
-
-var element = document.getElementById('musique8');
-
-element.addEventListener('click', function(e){
-  alert("lance la musique 8 chacal")
-});
-
-var element = document.getElementById('musique9');
-
-element.addEventListener('click', function(e){
-  alert("lance la musique 9 chacal")
-});
-
-var element = document.getElementById('musique10');
-
-element.addEventListener('click', function(e){
-  alert("lance la musique 10 chacal")
-});
-
-var element = document.getElementById('musique11');
-
-element.addEventListener('click', function(e){
-  alert("lance la musique 11 chacal")
-});
-
-var element = document.getElementById('musique12');
-
-element.addEventListener('click', function(e){
-  alert("lance la musique 12 chacal")
-});
-
-var element = document.getElementById('musique13');
-
-element.addEventListener('click', function(e){
-  alert("lance la musique 13 chacal")
-});
-
-var element = document.getElementById('musique14');
-
-element.addEventListener('click', function(e){
-  alert("lance la musique 14 chacal")
-});
-
-var element = document.getElementById('musique15');
-
-element.addEventListener('click', function(e){
-  alert("lance la musique 15 chacal")
-});
+    player.currentTime = 0;
+    player.pause();
+}
