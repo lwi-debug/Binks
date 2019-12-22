39459d7417384bf23d85f2d9dced9f681bab7420
GnomeApprenti <42942956+GnomeApprenti@users.noreply.github.com>
2019-12-22 16:29:48 +0100

    Reglages barre
    -On ne peut plus lancer deux musiques à la fois

--- Binks-master/Binks/siteNsi/script.js
+++ Binks-master/Binks/siteNsi/script.js
@@ -1,32 +1,41 @@
 function play(idPlayer, control) {
 
-  var player = document.querySelector('#' + idPlayer);
+  let player = document.querySelector('#' + idPlayer);
 
   if(player.paused){
     player.play();
-    control.textContent = 'pause';
   }
   else{
     player.pause();
-    control.textContent = 'play';
   }
 }
 
 function resume(idPlayer) {
 
-    var player = document.querySelector('#' + idPlayer);
+    let player = document.querySelector('#' + idPlayer);
 
     player.currentTime = 0;
     player.pause();
 }
 
+document.addEventListener('play', function(e){
+    let audios = document.getElementsByTagName('audio');
+    for(let i = 0, len = audios.length; i < len;i++){
+        if(audios[i] != e.target){
+            audios[i].pause();
+        }
+    }
+}, true);
+
+
 function update(player) {
-    var duration = player.duration;
-    var time     = player.currentTime;
-    var fraction = time / duration;
-    var percent  = fraction * 100;
 
-    var progress = document.querySelector('#barreProgression');
+  let barre = document.getElementById('barreProgression');
 
-    progress.style.width = percent + '%';
+  let duree = player.duration;
+  let temps = player.currentTime;
+  let fraction = temps/duree;
+  let pourcentage = fraction*100;
+
+  barre.style.width = pourcentage + '%';
 }
